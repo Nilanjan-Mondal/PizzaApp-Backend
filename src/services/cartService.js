676d3d21ae166b5c6d27bfd7c56869b7b751baa8
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-01-23 19:37:55 +0530

    Modified cart add and delete

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -9,7 +9,11 @@ async function getCart(userId) {
     return cart;
 }
 
- async function addToCart(userId, ProductId) {
+
+async function modifyCart(userId, ProductId, shouldAdd = true) {
+
+    const quantityValue = shouldAdd ? 1 : -1;
+
     const cart = await getCart(userId);
     const product = await getProductByIdRepository(ProductId);
     if(!product) {
@@ -23,20 +27,32 @@ async function getCart(userId) {
     let foundProduct = false;
     cart.items.forEach(item => {
         if(item.product._id == ProductId) {
-            if(product.quantity >= item.quantity) {
-                item.quantity += 1;
+            if(shouldAdd) {
+                if(product.quantity >= item.quantity) {
+                    item.quantity += quantityValue;
+                } else {
+                    throw new Error("The quantity of the item in the cart exceeds the available quantity in stock");
+                }
             } else {
-                throw new Error("The quantity of the item in the cart exceeds the available quantity in stock");
+                if(item.quantity > 0) {
+                    item.quantity += quantityValue;
+                } else {
+                    throw new Error("The quantity of the item in the cart is already 0");
+                }
             }
             
             foundProduct = true;
         }
     });
     if(!foundProduct) {
-        cart.items.push({
-            product: ProductId,
-            quantity: 1
-        });
+        if(shouldAdd) {
+            cart.items.push({
+                product: ProductId,
+                quantity: 1
+            });
+        } else {
+            throw new Error("Product not found in the cart");
+        }
     }
 
     await cart.save();
@@ -46,9 +62,48 @@ async function getCart(userId) {
     // await product.save();
 
     return cart;
- }
+}
+
+//  async function addToCart(userId, ProductId) {
+//     const cart = await getCart(userId);
+//     const product = await getProductByIdRepository(ProductId);
+//     if(!product) {
+//         throw new Error("Product not found");
+//     }
+//     if(!product.inStock && product.quantity <= 0) {
+//         throw new Error("Product out of stock");
+//     }
+
+//     // may be the product is already in the cart
+//     let foundProduct = false;
+//     cart.items.forEach(item => {
+//         if(item.product._id == ProductId) {
+//             if(product.quantity >= item.quantity) {
+//                 item.quantity += 1;
+//             } else {
+//                 throw new Error("The quantity of the item in the cart exceeds the available quantity in stock");
+//             }
+            
+//             foundProduct = true;
+//         }
+//     });
+//     if(!foundProduct) {
+//         cart.items.push({
+//             product: ProductId,
+//             quantity: 1
+//         });
+//     }
+
+//     await cart.save();
+
+//     // product.quantity -= 1;
+
+//     // await product.save();
+
+//     return cart;
+//  }
 
 module.exports = { 
     getCart,
-    addToCart
+    modifyCart
 };
